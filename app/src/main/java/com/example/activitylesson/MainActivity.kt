ecae742558743751a7768c5956516e05c7f14ae2
Ivan Voloshyn <ivan.voloshyn.fs@gmail.com>
2025-09-28 13:37:26 +0200

    Add lifecycle logging to Main Activity

--- app/src/main/java/com/example/activitylesson/MainActivity.kt
+++ app/src/main/java/com/example/activitylesson/MainActivity.kt
@@ -1,7 +1,59 @@
 package com.example.activitylesson
 
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+private const val TAG = "MAIN_ACTIVITY"
 
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private lateinit var btnFinish: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        btnFinish = findViewById(R.id.btn_finish)
+        log("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        log("OnStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        btnFinish.setOnClickListener {
+            finish()
+        }
+        log("OnResume")
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        log("OnPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        log("OnStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        log("OnRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("OnDestroy")
+    }
+
+    private fun log(message: String) {
+        Log.i(TAG, message)
+    }
 }
